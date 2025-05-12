a315514c8c0146d9ced0d5f3f53ed99fb6a8e707
vaibhavsharma308 <vs128646@gmail.com>
2025-05-13 02:01:12 +0530

    add deleteProduct end-point.

---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/CartItem.java
@@ -0,0 +1,32 @@
+package com.ecommerce.sb_ecom.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name ="product_id" ,nullable = false )
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
