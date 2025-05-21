eee3108efcc1dc8ada7521f48d7a7e25878e752e
vaibhavsharma308 <vs128646@gmail.com>
2025-05-21 21:45:26 +0530

    Start writing testcases using Junit5 and mockito

---
+++ sb-ecom/src/test/java/com/ecommerce/sb_ecom/service/UserServiceTest.java
@@ -0,0 +1,60 @@
+package com.ecommerce.sb_ecom.service;
+
+
+import com.ecommerce.sb_ecom.model.Address;
+import com.ecommerce.sb_ecom.model.User;
+import com.ecommerce.sb_ecom.model.UserRole;
+import com.ecommerce.sb_ecom.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+
+
+
+    public static User createMockUser() {
+        Address mockAddress = new Address();
+        mockAddress.setId(1L);
+        mockAddress.setStreet("123 Main St");
+        mockAddress.setCity("New York");
+        mockAddress.setState("NY");
+        mockAddress.setZipcode("10001");
+
+
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setFirstName("John");
+        mockUser.setLastName("Doe");
+        mockUser.setEmail("john.doe@example.com");
+        mockUser.setPhone("1234567890");
+        mockUser.setRole(UserRole.CUSTOMER);
+        mockUser.setAddress(mockAddress);
+        mockUser.setCreatedAt(LocalDateTime.now());
+        mockUser.setUpdatedAt(LocalDateTime.now());
+        return mockUser;
+    }
+    @Test
+    void createUserTest() {
+        User user = createMockUser() ;
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        User addedUser = userService.createUser(user);
+        Assertions.assertEquals(addedUser,user);
+    }
+    @Test
+    void runTest(){
+        System.out.println("Running our first Test case");
+    }
+}
