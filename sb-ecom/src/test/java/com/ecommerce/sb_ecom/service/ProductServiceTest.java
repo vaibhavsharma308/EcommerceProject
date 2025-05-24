cd7144214bd91bc3d1074bd0e1b7e379cab3082e
vaibhavsharma308 <vs128646@gmail.com>
2025-05-24 23:52:31 +0530

    Write testcase for test productById method.

--- sb-ecom/src/test/java/com/ecommerce/sb_ecom/service/ProductServiceTest.java
+++ sb-ecom/src/test/java/com/ecommerce/sb_ecom/service/ProductServiceTest.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -62,5 +63,32 @@ public class ProductServiceTest {
         List<ProductResponse> actualProductResponse=productService.fetchProducts();
         Assertions.assertNotNull(actualProductResponse);
         Assertions.assertEquals(Arrays.asList(productResponse),actualProductResponse);
+    }
+
+    @Test
+    void testProductById_success(){
+        Long Id = 1L;
+        Product product = new Product();
+        product.setId(Id);
+        product.setName("Test Product");
+        ProductResponse exceptedProductResponse = new ProductResponse();
+        exceptedProductResponse.setName("Test Product");
+        Mockito.when(productRepository.findById(Id)).thenReturn(Optional.of(product));
+        Mockito.when(productMapper.productToProductResponse(product)).thenReturn(exceptedProductResponse);
+        ProductResponse actualProductResponse = productService.productById(Id);
+        Assertions.assertNotNull(actualProductResponse);
+        Assertions.assertEquals(exceptedProductResponse,actualProductResponse);
+        Assertions.assertEquals(exceptedProductResponse.getName(),actualProductResponse.getName());
+    }
+
+    @Test
+    void testProductById_ExceptionCase(){
+        ProductResponse exceptedProductResponse = new ProductResponse();
+        Long productId = 2L;
+        Mockito.when(productRepository.findById(productId)).thenReturn(Optional.empty());
+        RuntimeException runtimeException= Assertions.assertThrows(RuntimeException.class,()->{
+            productService.productById(productId);
+        });
+        Assertions.assertEquals("Product not found with id: " + productId,runtimeException.getMessage());
     }
 }
